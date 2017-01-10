ff83507be5aeb4f23f7413fa7639f7aa1197a66c
Private <ZaakCH>
2017-01-10 13:20:23 +0100

    2nd after 2nd fail

--- Helloworld.java
+++ Helloworld.java
@@ -3,9 +3,9 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
-		System.out.println("Hello Jenkins!");
-		System.out.println("Done  !");
+		System.out.println("Hello World !");
+		System.out.println("Hello Jenkins !");
+		System.out.println("Done !");
 		
 	}
 
